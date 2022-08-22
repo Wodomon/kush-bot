0361899b839d6e55c0d3538934ff01038efa5c2c
Wodomon <wodomons@gmail.com>
2022-08-22 22:30:37 +0300

    config example

---
+++ config.example.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'key' => 'your telegram api key',
+    'users' => [
+        // user ids to be filtered
+    ],
+    'chats' => [
+        // chat ids to be filtered
+    ],
+    'logs' => [
+        // chat ids to be logged here
+    ],
+    'url' => 'url to kush.php to setup webhook'
+];
